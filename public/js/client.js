494ab4edd1eb8845c1bb53811c663448bba932b2
Colin <vogon@outlook.com>
2013-12-22 12:57:20 -0800

    colors

--- public/js/client.js
+++ public/js/client.js
@@ -2,18 +2,32 @@
     draw = canvas.getContext("2d"),
     Life = life.Life;
 
-var displayW = canvas.offsetWidth,
-    displayH = canvas.offsetHeight;
+var layoutW = canvas.offsetWidth,
+    layoutH = canvas.offsetHeight;
+
+var worldW = Math.floor(layoutW / 2),
+    worldH = Math.floor(layoutH / 2);
+
+var displayW = worldW * 2, displayH = worldH * 2;
 
 canvas.width = displayW;
 canvas.height = displayH;
 canvas.style.width = displayW + 'px';
 canvas.style.height = displayH + 'px';
 
-var world = new Life(displayW, displayH);
+var world = new Life(worldW, worldH);
 
 var glider = new Int32Array(9);
 glider[1] = glider[5] = glider[6] = glider[7] = glider[8] = 0xffffffff;
+
+function putPixel(imageData, x, y, r, g, b, a) {
+  var offset = (y * imageData.width + x) * 4;
+
+  imageData.data[offset] = r;
+  imageData.data[offset + 1] = g;
+  imageData.data[offset + 2] = b;
+  imageData.data[offset + 3] = a;
+}
 
 function paint() {
   // clear
@@ -21,20 +35,36 @@ function paint() {
   draw.fillRect(0, 0, displayW, displayH);
 
   // draw live cells
-  for (var y = 0; y < displayH; y++) {
-    for (var x = 0; x < displayW; x++) {
-      if (world.cellAt(x, y) != 0) {
-        draw.fillStyle = 'black';
-        draw.fillRect(x, y, 1, 1);
+  img = draw.createImageData(displayW, displayH);
+
+  for (var y = 0; y < worldH; y++) {
+    for (var x = 0; x < worldW; x++) {
+      var r, g, b, a, color = world.cellAt(x, y);
+
+      if (color != 0) {
+        //debugger;
       }
+
+      r = color & 0xff;
+      g = (color & 0xff00) >>> 8;
+      b = (color & 0xff0000) >>> 16;
+      a = (color & 0xff000000) >>> 24;
+
+      putPixel(img, x * 2, y * 2, r, g, b, a);
+      putPixel(img, x * 2 + 1, y * 2, r, g, b, a);
+      putPixel(img, x * 2, y * 2 + 1, r, g, b, a);
+      putPixel(img, x * 2 + 1, y * 2 + 1, r, g, b, a);
     }
   }
 
-  // blit a new glider in
-  var blitX = Math.floor(Math.random() * displayW),
-      blitY = Math.floor(Math.random() * displayH);
+  draw.putImageData(img, 0, 0);
 
-  world.blit(glider, 3, 3, blitX, blitY);
+  // blit a new glider in
+  var blitX = Math.floor(Math.random() * worldW),
+      blitY = Math.floor(Math.random() * worldH);
+  var color = 0x7f000000 | Math.floor(Math.random() * 0xffffff);
+
+  world.blit(glider, 3, 3, blitX, blitY, color);
 
   // update
   world.next();
--- shared/life.js
+++ shared/life.js
@@ -22,21 +22,44 @@ function coords2Offset(x, y, w, h) {
 }
 
 function nextCellInView(view, x, y, w, h) {
-  var current = isAlive(cellInView(view, x, y, w, h));
-  var count =
-    isAlive(cellInView(view, x - 1, y - 1, w, h)) +
-    isAlive(cellInView(view, x,     y - 1, w, h)) +
-    isAlive(cellInView(view, x + 1, y - 1, w, h)) +
-    isAlive(cellInView(view, x - 1, y,     w, h)) +
-    isAlive(cellInView(view, x + 1, y,     w, h)) +
-    isAlive(cellInView(view, x - 1, y + 1, w, h)) +
-    isAlive(cellInView(view, x,     y + 1, w, h)) +
-    isAlive(cellInView(view, x + 1, y + 1, w, h));
+  var current = cellInView(view, x, y, w, h);
+  var r = 0, g = 0, b = 0, count = 0;
 
-  if (current) {
-    return (count == 2 || count == 3) ? 0xffffffff : 0x0;
-  } else {
-    return (count == 3) ? 0xffffffff : 0x0;
+  for (var ny = y - 1; ny <= y + 1; ny++) {
+    for (var nx = x - 1; nx <= x + 1; nx++) {
+      // don't count this cell
+      if (nx == x && ny == y) continue;
+
+      var neighbor = cellInView(view, nx, ny, w, h);
+
+      if (isAlive(neighbor)) {
+        count++;
+        // ABGR color format
+        r += (neighbor & 0xff) / 3;
+        g += ((neighbor & 0xff00) >>> 8) / 3;
+        b += ((neighbor & 0xff0000) >>> 16) / 3;
+      }
+
+      // check for overcrowding
+      if (count > 3) return 0x0;
+    }
+  }
+
+  if (count < 2) {
+    // cell is lonely and dies
+    return 0x0;
+  } else if (count == 2) {
+    // currently-living cells retain their color; currently-dead cells stay dead
+    return current;
+  } else /* if (count == 3) */ {
+    if (isAlive(current)) {
+      // currently-living cells retain their color
+      return current;
+    } else {
+      // currently-dead cells are born and get the average color of their living "parents"
+      var color = 0xff000000 | (Math.floor(b) << 16) | (Math.floor(g) << 8) | Math.floor(r);
+      return color;
+    }
   }
 }
 
@@ -44,11 +67,8 @@ function Life(w, h) {
   this._w = w;
   this._h = h;
 
-  this._buffer1 = new ArrayBuffer(w * h * cellSize);
-  this._view1 = new view(this._buffer1);
-
-  this._buffer2 = new ArrayBuffer(w * h * cellSize);
-  this._view2 = new view(this._buffer2);
+  this._view1 = new view(w * h);
+  this._view2 = new view(w * h);
 
   this._front = this._view1;
   this._back = this._view2;
@@ -70,11 +90,16 @@ Life.prototype.next = function() {
   this._back = wasFront;
 }
 
-Life.prototype.blit = function(sprite, spriteW, spriteH, x, y) {
+Life.prototype.blit = function(sprite, spriteW, spriteH, x, y, tint) {
   for (var sy = 0; sy < spriteH; sy++) {
     for (var sx = 0; sx < spriteW; sx++) {
       var wx = x + sx, wy = y + sy;
-      this._front[coords2Offset(wx, wy, this._w, this._h)] = sprite[coords2Offset(sx, sy, spriteW, spriteH)];
+
+      if (isAlive(sprite[coords2Offset(sx, sy, spriteW, spriteH)])) {
+        this._front[coords2Offset(wx, wy, this._w, this._h)] = tint;
+      } else {
+        this._front[coords2Offset(wx, wy, this._w, this._h)] = 0x0;
+      }
     }
   }
 }
